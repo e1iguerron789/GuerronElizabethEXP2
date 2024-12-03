621d04407ac5efb77e6fd9c941b0907fc2119203
Johao_G10 <JG!jg3651+>
2024-12-03 07:20:37 -0500

    Agregar archivos de proyecto.

---
+++ GuerronElizabethEXP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GuerronElizabethEXP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GuerronElizabethEXP2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GuerronElizabethEXP2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
